9b95720de79b25194c3d921a8ba6f1a1f47ec654
Mitiku Yohannes <se.mitiku.yohannes@gmail.com>
2024-12-31 23:35:35 +0000

    Add more information to release script

--- release.sh
+++ release.sh
@@ -1,3 +1,34 @@
+CONDA_DEFAULT_ENV=fluxon-env
+
+# Helper function for printing a status message
+print_status() {
+    echo -e "${YELLOW}[STATUS]${NC} $1"
+}
+
+# Print a header
+echo -e "${GREEN}=================================="
+echo -e "Running Fluxon Release"
+echo -e "==================================${NC}"
+
+# Check for conda environment
+if ! command -v conda &> /dev/null; then
+    echo -e "${RED}[ERROR] Conda not found! Please install Anaconda or Miniconda.${NC}"
+    exit 1
+else 
+    print_status "Conda is installed"
+    print_status "Activating Conda environment: $CONDA_DEFAULT_ENV"
+    source activate $CONDA_DEFAULT_ENV
+fi
+
+# Check if an environment is activated
+if [ -z "$CONDA_DEFAULT_ENV" ]; then
+    echo -e "${RED}[ERROR] No Conda environment is active! Activate an environment and try again.${NC}"
+    exit 1
+else
+    print_status "Using Conda environment: $CONDA_DEFAULT_ENV"
+fi
+
+
 git checkout main
 git pull
 current_version=$(git describe --tags --abbrev=0)
@@ -15,11 +46,11 @@ new_version=$(git describe --tags --abbrev=0)
 echo "New version: $new_version"
 
 # Commit and push
-git push --tags $new_version
+git push origin --tags $new_version
 git push origin main
 
 # Create release
-gh release create $new_version --target main --title $new_version --notes "Release $new_version"
+gh release create v0.0.3 --target main --title v0.0.3 --notes "Release v0.0.3"
 
 # Build and upload to PyPI
 python setup.py sdist bdist_wheel
